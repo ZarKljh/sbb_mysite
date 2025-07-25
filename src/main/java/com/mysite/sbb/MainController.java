5f35b20147a0b70d451462d751037a46ec0ac473
ZarKljh <zarkljh@gmail.com>
2025-07-25 16:07:40 +0900

    H2 DB ì±Connect

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -1,4 +1,17 @@
 package com.mysite.sbb;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MainController {
+
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index(){
+        return String.format("안녕하세요 SBB에 오신것을 환영합니다");
+    }
+
 }
