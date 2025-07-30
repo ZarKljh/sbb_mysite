7816ef504f2ba3d72d5604d0a59ba29d781462f0
ZarKljh <zarkljh@gmail.com>
2025-07-30 18:14:03 +0900

    rootUrl add recommit

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    // ("/") redirect에서 지정하는 요청으로 자동으로 넘어간다는 의미
-    @GetMapping("/")
+
+    @GetMapping("/sbb")
     @ResponseBody
     public String index()
     {
+        return String.format("안녕하세요 sbb에 오신것을 환영합니다.");
+    }
+
+    // ("/") redirect에서 지정하는 요청으로 자동으로 넘어간다는 의미
+    @GetMapping("/")
+    public String root(){
         return "redirect:/question/list";
     }
 
