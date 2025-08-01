a9af9bc98e9e8f6b3b22288b37ee0d2a0446e09e
ZarKljh <zarkljh@gmail.com>
2025-08-01 16:22:02 +0900

    answerForm add

--- src/main/java/com/mysite/sbb/Question/QuestionController.java
+++ src/main/java/com/mysite/sbb/Question/QuestionController.java
@@ -1,9 +1,15 @@
 package com.mysite.sbb.Question;
 
+import com.mysite.sbb.Answer.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -29,7 +35,7 @@ public class QuestionController {
         return "question_list";
     }
     @GetMapping(value = "/detail/{id}")
-    public String getQuestion(Model model, @PathVariable("id") Integer id){
+    public String getQuestion(Model model, @PathVariable("id") Integer id, AnswerForm answerForm){
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
@@ -41,13 +47,16 @@ public class QuestionController {
 //        return "Answer create Complete";
 //    }
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
     @PostMapping("/create")
-    public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
-        // TODO 질문을 저장한다.
-        this.questionService.create(subject, content);
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
