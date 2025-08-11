91ea506f096b38a246410e93f37192febe435b29
ZarKljh <zarkljh@gmail.com>
2025-08-11 14:47:59 +0900

    anwer modify Add

--- src/main/java/com/mysite/sbb/Answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/Answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.mysite.sbb.Answer;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.Question.Question;
 import com.mysite.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,6 +21,20 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setCreateDate(LocalDateTime.now());
         answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+    public Answer getAnswer(Integer id){
+        Optional<Answer> oa = this.answerRepository.findById(id);
+
+        if(oa.isPresent()){
+            return oa.get();
+        } else {
+            throw new DataNotFoundException("answer is not founded");
+        }
+    }
+    public void modify(Answer answer, String content){
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
 }
