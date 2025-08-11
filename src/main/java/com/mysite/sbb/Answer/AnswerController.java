558bc37f3f45404fc89e1414deaa63470c94425d
ZarKljh <zarkljh@gmail.com>
2025-08-11 15:12:46 +0900

    answer delete ADD

--- src/main/java/com/mysite/sbb/Answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/Answer/AnswerController.java
@@ -46,7 +46,7 @@ public class AnswerController {
     public String modifyAnswer(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal) {
         Answer answer = this.answerService.getAnswer(id);
         if(!answer.getAuthor().getUsername().equals(principal.getName())){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"삭제권한이 없습니다");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"수정권한이 없습니다");
         }
         answerForm.setContent(answer.getContent());
         return "answer_form";
@@ -65,4 +65,15 @@ public class AnswerController {
         this.answerService.modify(answer, answerForm.getContent());
         return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
     }
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String deleteAnswer(Principal principal, @PathVariable("id") Integer id){
+        Answer answer = this.answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"삭제권한이 없습니다");
+        }
+        this.answerService.delete(answer);
+        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+    }
+
 }
